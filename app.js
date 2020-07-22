c32b2e293c09d581c2a240f9d46549e3b6c793a3
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2020-07-22 14:50:36 -0600

    Object Onductructing

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-function Tarea(nombre, urgencia) {
-    this.nombre = nombre;
-    this.urgencia = urgencia;
+const aprendiendoJS = {
+    varsion: {
+        nueva : 'ES6',
+        anterior : 'ES5'
+    },
+    frameworks : ['React', 'VueJS', 'AngularJS']
 }
 
-Tarea.prototype.mostrarInformacionTarea = function() {
-    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
-}
-
-const tarea1 = new Tarea('Aprender JavaScript y React', 'Urgente');
-const tarea2 = new Tarea('Pasear al perro', 'Media');
-
-console.log(tarea1);
-console.log(tarea1.mostrarInformacionTarea());
-console.log(tarea2);
-console.log(tarea2.mostrarInformacionTarea());
+let {anterior} = aprendiendoJS.varsion;
+console.log(anterior);
