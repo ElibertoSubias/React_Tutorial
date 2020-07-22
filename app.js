3909d967ace02282caf7db829363b776a97370ac
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2020-07-22 15:09:28 -0600

    Spread Operator

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const persona = {
-    nombre : 'Juan',
-    profesion : 'Desarrollador Web',
-    edad : 26
+let lenguajes = ['JavaScript', 'PHP', 'Python'];
+//let frameworks = ['React', 'Laravel', 'Django'];
+
+//Anterior
+//let combinacion = lenguajes.concat(frameworks);
+
+//Nueva
+//let combinacion = [...lenguajes,...frameworks];
+
+//Clonar Objeto 
+// let [ultimo] = [...lenguajes].reverse();
+// console.log(lenguajes);
+// console.log(ultimo);
+
+
+function suma(a,b,c){
+    console.log(a+b+c);
 }
 
-console.log(Object.keys(persona));
+const numeros = [1,2,3];
+
+suma(...numeros);
